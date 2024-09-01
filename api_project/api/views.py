76368d403c0861652d2b88625f1bb9158ccb5737
Princewill Smithon <onumaduprincewill@gmail.com>
2024-09-01 17:50:05 +0100

    API endpoint using REST framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from.models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
